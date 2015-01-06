b3a9c1128e82f2af622abf8309808a66e12d0ddc
Rob Eisenberg <rob@bluespire.com>
2015-01-06 14:11:35 -0500

    feat(build): update compiler and switch to register module format

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "aurelia-binding/*": "dist/*.js"
   }
 });
 
 System.config({
   "map": {
-    "aurelia-metadata": "github:aurelia/metadata@0.1.1",
-    "aurelia-task-queue": "github:aurelia/task-queue@0.1.1"
+    "aurelia-metadata": "github:aurelia/metadata@0.2.3",
+    "aurelia-task-queue": "github:aurelia/task-queue@0.2.0"
   }
 });
 
