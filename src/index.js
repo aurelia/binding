4abe921a23af328ef30567883cde224763e01ed5
Rob Eisenberg <rob@bluespire.com>
2015-01-21 01:27:13 -0500

    refactor(ast): unify ast

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import {patchAST} from './ast';
-patchAST();
-
 export {EventManager} from './event-manager';
 export {ObserverLocator} from './observer-locator';
 export {ValueConverter} from './value-converter';
 export {calcSplices} from './array-change-records';
 export * from './binding-modes';
-export {Parser} from './expressions/parser';
+export {Parser} from './parser';
 export {BindingExpression} from './binding-expression';
 export {ListenerExpression} from './listener-expression';
 export {NameExpression} from './name-expression';
