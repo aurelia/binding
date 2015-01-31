43ca381823654727fd4a75e76906076f93034226
Ashley Grant <ashley@ashleygrant.com>
2015-01-30 21:13:42 -0500

    fix(binding): make global functions accessible in templates
    fixes #11

--- src/ast.js
+++ src/ast.js
@@ -970,7 +970,7 @@ function autoConvertAdd(a, b) {
 }
 
 function ensureFunctionFromMap(obj, name){
-  var func = obj[name];
+  var func = obj[name] || window[name];
 
   if (typeof func === 'function') {
     return func;
