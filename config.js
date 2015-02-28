b3ffcd5fd378a8ae4bce54c1949581447b5e2d92
Rob Eisenberg <rob@bluespire.com>
2015-02-28 00:15:15 -0500

    fix(package): update dependencies

--- config.js
+++ config.js
@@ -9,18 +9,18 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.1",
-    "aurelia-metadata": "github:aurelia/metadata@0.3.0",
-    "aurelia-task-queue": "github:aurelia/task-queue@0.2.2",
-    "github:aurelia/dependency-injection@0.4.1": {
-      "aurelia-metadata": "github:aurelia/metadata@0.3.0",
+    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.4",
+    "aurelia-metadata": "github:aurelia/metadata@0.3.2",
+    "aurelia-task-queue": "github:aurelia/task-queue@0.2.4",
+    "github:aurelia/dependency-injection@0.4.4": {
+      "aurelia-metadata": "github:aurelia/metadata@0.3.2",
       "core-js": "npm:core-js@0.4.10"
     },
-    "github:jspm/nodelibs-process@0.1.0": {
+    "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.0"
     },
     "npm:core-js@0.4.10": {
-      "process": "github:jspm/nodelibs-process@0.1.0"
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
