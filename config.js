91c6f8d18380c45222ae996df3c668da782f04d2
Rob Eisenberg <rob@bluespire.com>
2015-02-28 19:48:21 -0500

    fix(package): change jspm directories

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.4",
-    "aurelia-metadata": "github:aurelia/metadata@0.3.2",
-    "aurelia-task-queue": "github:aurelia/task-queue@0.2.4",
-    "github:aurelia/dependency-injection@0.4.4": {
-      "aurelia-metadata": "github:aurelia/metadata@0.3.2",
+    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.5",
+    "aurelia-metadata": "github:aurelia/metadata@0.3.3",
+    "aurelia-task-queue": "github:aurelia/task-queue@0.2.5",
+    "github:aurelia/dependency-injection@0.4.5": {
+      "aurelia-metadata": "github:aurelia/metadata@0.3.3",
       "core-js": "npm:core-js@0.4.10"
     },
     "github:jspm/nodelibs-process@0.1.1": {
