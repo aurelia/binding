1911444b3f27657f254b50c965b4f9a7fdf99038
Rob Eisenberg <rob@bluespire.com>
2015-04-09 02:08:03 -0400

    chore(git): add missed files

---
+++ dist/commonjs/property-observation.js
@@ -0,0 +1,358 @@
+'use strict';
+
+var _interopRequireWildcard = function (obj) { return obj && obj.__esModule ? obj : { 'default': obj }; };
+
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } };
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _core = require('core-js');
+
+var _core2 = _interopRequireWildcard(_core);
+
+var SetterObserver = (function () {
+  function SetterObserver(taskQueue, obj, propertyName) {
+    _classCallCheck(this, SetterObserver);
+
+    this.taskQueue = taskQueue;
+    this.obj = obj;
+    this.propertyName = propertyName;
+    this.callbacks = [];
+    this.queued = false;
+    this.observing = false;
+  }
+
+  _createClass(SetterObserver, [{
+    key: 'getValue',
+    value: function getValue() {
+      return this.obj[this.propertyName];
+    }
+  }, {
+    key: 'setValue',
+    value: function setValue(newValue) {
+      this.obj[this.propertyName] = newValue;
+    }
+  }, {
+    key: 'getterValue',
+    value: function getterValue() {
+      return this.currentValue;
+    }
+  }, {
+    key: 'setterValue',
+    value: function setterValue(newValue) {
+      var oldValue = this.currentValue;
+
+      if (oldValue != newValue) {
+        if (!this.queued) {
+          this.oldValue = oldValue;
+          this.queued = true;
+          this.taskQueue.queueMicroTask(this);
+        }
+
+        this.currentValue = newValue;
+      }
+    }
+  }, {
+    key: 'call',
+    value: function call() {
+      var callbacks = this.callbacks,
+          i = callbacks.length,
+          oldValue = this.oldValue,
+          newValue = this.currentValue;
+
+      this.queued = false;
+
+      while (i--) {
+        callbacks[i](newValue, oldValue);
+      }
+    }
+  }, {
+    key: 'subscribe',
+    value: function subscribe(callback) {
+      var callbacks = this.callbacks;
+      callbacks.push(callback);
+
+      if (!this.observing) {
+        this.convertProperty();
+      }
+
+      return function () {
+        callbacks.splice(callbacks.indexOf(callback), 1);
+      };
+    }
+  }, {
+    key: 'convertProperty',
+    value: function convertProperty() {
+      this.observing = true;
+      this.currentValue = this.obj[this.propertyName];
+      this.setValue = this.setterValue;
+      this.getValue = this.getterValue;
+
+      try {
+        Object.defineProperty(this.obj, this.propertyName, {
+          configurable: true,
+          enumerable: true,
+          get: this.getValue.bind(this),
+          set: this.setValue.bind(this)
+        });
+      } catch (_) {}
+    }
+  }]);
+
+  return SetterObserver;
+})();
+
+exports.SetterObserver = SetterObserver;
+
+var OoObjectObserver = (function () {
+  function OoObjectObserver(obj, observerLocator) {
+    _classCallCheck(this, OoObjectObserver);
+
+    this.obj = obj;
+    this.observers = {};
+    this.observerLocator = observerLocator;
+  }
+
+  _createClass(OoObjectObserver, [{
+    key: 'subscribe',
+    value: function subscribe(propertyObserver, callback) {
+      var _this = this;
+
+      var callbacks = propertyObserver.callbacks;
+      callbacks.push(callback);
+
+      if (!this.observing) {
+        this.observing = true;
+        try {
+          Object.observe(this.obj, function (changes) {
+            return _this.handleChanges(changes);
+          }, ['update', 'add']);
+        } catch (_) {}
+      }
+
+      return function () {
+        callbacks.splice(callbacks.indexOf(callback), 1);
+      };
+    }
+  }, {
+    key: 'getObserver',
+    value: function getObserver(propertyName, descriptor) {
+      var propertyObserver = this.observers[propertyName];
+      if (!propertyObserver) {
+        if (descriptor) {
+          propertyObserver = this.observers[propertyName] = new OoPropertyObserver(this, this.obj, propertyName);
+        } else {
+          propertyObserver = this.observers[propertyName] = new UndefinedPropertyObserver(this, this.obj, propertyName);
+        }
+      }
+      return propertyObserver;
+    }
+  }, {
+    key: 'handleChanges',
+    value: function handleChanges(changeRecords) {
+      var updates = {},
+          observers = this.observers,
+          i = changeRecords.length;
+
+      while (i--) {
+        var change = changeRecords[i],
+            name = change.name;
+
+        if (!(name in updates)) {
+          var observer = observers[name];
+          updates[name] = true;
+          if (observer) {
+            observer.trigger(change.object[name], change.oldValue);
+          }
+        }
+      }
+    }
+  }]);
+
+  return OoObjectObserver;
+})();
+
+exports.OoObjectObserver = OoObjectObserver;
+
+var OoPropertyObserver = (function () {
+  function OoPropertyObserver(owner, obj, propertyName) {
+    _classCallCheck(this, OoPropertyObserver);
+
+    this.owner = owner;
+    this.obj = obj;
+    this.propertyName = propertyName;
+    this.callbacks = [];
+  }
+
+  _createClass(OoPropertyObserver, [{
+    key: 'getValue',
+    value: function getValue() {
+      return this.obj[this.propertyName];
+    }
+  }, {
+    key: 'setValue',
+    value: function setValue(newValue) {
+      this.obj[this.propertyName] = newValue;
+    }
+  }, {
+    key: 'trigger',
+    value: function trigger(newValue, oldValue) {
+      var callbacks = this.callbacks,
+          i = callbacks.length;
+
+      while (i--) {
+        callbacks[i](newValue, oldValue);
+      }
+    }
+  }, {
+    key: 'subscribe',
+    value: function subscribe(callback) {
+      return this.owner.subscribe(this, callback);
+    }
+  }]);
+
+  return OoPropertyObserver;
+})();
+
+exports.OoPropertyObserver = OoPropertyObserver;
+
+var UndefinedPropertyObserver = (function () {
+  function UndefinedPropertyObserver(owner, obj, propertyName) {
+    _classCallCheck(this, UndefinedPropertyObserver);
+
+    this.owner = owner;
+    this.obj = obj;
+    this.propertyName = propertyName;
+    this.callbackMap = new Map();
+    this.callbacks = [];
+  }
+
+  _createClass(UndefinedPropertyObserver, [{
+    key: 'getValue',
+    value: function getValue() {
+      if (this.actual) {
+        return this.actual.getValue();
+      }
+      return this.obj[this.propertyName];
+    }
+  }, {
+    key: 'setValue',
+    value: function setValue(newValue) {
+      if (this.actual) {
+        this.actual.setValue(newValue);
+        return;
+      }
+
+      this.obj[this.propertyName] = newValue;
+      this.trigger(newValue, undefined);
+    }
+  }, {
+    key: 'trigger',
+    value: function trigger(newValue, oldValue) {
+      var callback;
+
+      if (this.subscription) {
+        this.subscription();
+      }
+
+      this.getObserver();
+
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = this.callbackMap.keys()[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          callback = _step.value;
+
+          callback(newValue, oldValue);
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator['return']) {
+            _iterator['return']();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+    }
+  }, {
+    key: 'getObserver',
+    value: function getObserver() {
+      var callback, observerLocator;
+
+      if (!Object.getOwnPropertyDescriptor(this.obj, this.propertyName)) {
+        return;
+      }
+
+      observerLocator = this.owner.observerLocator;
+      delete this.owner.observers[this.propertyName];
+      delete observerLocator.getObserversLookup(this.obj, observerLocator)[this.propertyName];
+      this.actual = observerLocator.getObserver(this.obj, this.propertyName);
+
+      var _iteratorNormalCompletion2 = true;
+      var _didIteratorError2 = false;
+      var _iteratorError2 = undefined;
+
+      try {
+        for (var _iterator2 = this.callbackMap.keys()[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+          callback = _step2.value;
+
+          this.callbackMap.set(callback, this.actual.subscribe(callback));
+        }
+      } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion2 && _iterator2['return']) {
+            _iterator2['return']();
+          }
+        } finally {
+          if (_didIteratorError2) {
+            throw _iteratorError2;
+          }
+        }
+      }
+    }
+  }, {
+    key: 'subscribe',
+    value: function subscribe(callback) {
+      var _this2 = this;
+
+      if (!this.actual) {
+        this.getObserver();
+      }
+
+      if (this.actual) {
+        return this.actual.subscribe(callback);
+      }
+
+      if (!this.subscription) {
+        this.subscription = this.owner.subscribe(this);
+      }
+
+      this.callbackMap.set(callback, null);
+
+      return function () {
+        var actualDispose = _this2.callbackMap.get(callback);
+        if (actualDispose) actualDispose();
+        _this2.callbackMap['delete'](callback);
+      };
+    }
+  }]);
+
+  return UndefinedPropertyObserver;
+})();
+
+exports.UndefinedPropertyObserver = UndefinedPropertyObserver;
---
+++ dist/system/event-manager.js
@@ -0,0 +1,201 @@
+System.register([], function (_export) {
+  var _classCallCheck, _createClass, DefaultEventStrategy, EventManager;
+
+  return {
+    setters: [],
+    execute: function () {
+      'use strict';
+
+      _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } };
+
+      _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+      DefaultEventStrategy = (function () {
+        function DefaultEventStrategy() {
+          _classCallCheck(this, DefaultEventStrategy);
+
+          this.delegatedEvents = {};
+        }
+
+        _createClass(DefaultEventStrategy, [{
+          key: 'ensureDelegatedEvent',
+          value: function ensureDelegatedEvent(eventName) {
+            if (this.delegatedEvents[eventName]) {
+              return;
+            }
+
+            this.delegatedEvents[eventName] = true;
+            document.addEventListener(eventName, this.handleDelegatedEvent.bind(this), false);
+          }
+        }, {
+          key: 'handleCallbackResult',
+          value: function handleCallbackResult(result) {}
+        }, {
+          key: 'handleDelegatedEvent',
+          value: function handleDelegatedEvent(event) {
+            event = event || window.event;
+            var target = event.target || event.srcElement,
+                callback;
+
+            while (target && !callback) {
+              if (target.delegatedEvents) {
+                callback = target.delegatedEvents[event.type];
+              }
+
+              if (!callback) {
+                target = target.parentNode;
+              }
+            }
+
+            if (callback) {
+              this.handleCallbackResult(callback(event));
+            }
+          }
+        }, {
+          key: 'createDirectEventCallback',
+          value: function createDirectEventCallback(callback) {
+            var _this = this;
+
+            return function (event) {
+              _this.handleCallbackResult(callback(event));
+            };
+          }
+        }, {
+          key: 'subscribeToDelegatedEvent',
+          value: function subscribeToDelegatedEvent(target, targetEvent, callback) {
+            var lookup = target.delegatedEvents || (target.delegatedEvents = {});
+
+            this.ensureDelegatedEvent(targetEvent);
+            lookup[targetEvent] = callback;
+
+            return function () {
+              lookup[targetEvent] = null;
+            };
+          }
+        }, {
+          key: 'subscribeToDirectEvent',
+          value: function subscribeToDirectEvent(target, targetEvent, callback) {
+            var directEventCallback = this.createDirectEventCallback(callback);
+            target.addEventListener(targetEvent, directEventCallback, false);
+
+            return function () {
+              target.removeEventListener(targetEvent, directEventCallback);
+            };
+          }
+        }, {
+          key: 'subscribe',
+          value: function subscribe(target, targetEvent, callback, delegate) {
+            if (delegate) {
+              return this.subscribeToDirectEvent(target, targetEvent, callback);
+            } else {
+              return this.subscribeToDelegatedEvent(target, targetEvent, callback);
+            }
+          }
+        }]);
+
+        return DefaultEventStrategy;
+      })();
+
+      EventManager = (function () {
+        function EventManager() {
+          _classCallCheck(this, EventManager);
+
+          this.elementHandlerLookup = {};
+          this.eventStrategyLookup = {};
+
+          this.registerElementConfig({
+            tagName: 'input',
+            properties: {
+              value: ['change', 'input'],
+              checked: ['change', 'input']
+            }
+          });
+
+          this.registerElementConfig({
+            tagName: 'textarea',
+            properties: {
+              value: ['change', 'input']
+            }
+          });
+
+          this.registerElementConfig({
+            tagName: 'select',
+            properties: {
+              value: ['change']
+            }
+          });
+
+          this.defaultEventStrategy = new DefaultEventStrategy();
+        }
+
+        _createClass(EventManager, [{
+          key: 'registerElementConfig',
+          value: function registerElementConfig(config) {
+            var tagName = config.tagName.toLowerCase(),
+                properties = config.properties,
+                propertyName;
+            this.elementHandlerLookup[tagName] = {};
+            for (propertyName in properties) {
+              if (properties.hasOwnProperty(propertyName)) {
+                this.registerElementPropertyConfig(tagName, propertyName, properties[propertyName]);
+              }
+            }
+          }
+        }, {
+          key: 'registerElementPropertyConfig',
+          value: function registerElementPropertyConfig(tagName, propertyName, events) {
+            this.elementHandlerLookup[tagName][propertyName] = {
+              subscribe: function subscribe(target, callback) {
+                events.forEach(function (changeEvent) {
+                  target.addEventListener(changeEvent, callback, false);
+                });
+
+                return function () {
+                  events.forEach(function (changeEvent) {
+                    target.removeEventListener(changeEvent, callback);
+                  });
+                };
+              }
+            };
+          }
+        }, {
+          key: 'registerElementHandler',
+          value: function registerElementHandler(tagName, handler) {
+            this.elementHandlerLookup[tagName.toLowerCase()] = handler;
+          }
+        }, {
+          key: 'registerEventStrategy',
+          value: function registerEventStrategy(eventName, strategy) {
+            this.eventStrategyLookup[eventName] = strategy;
+          }
+        }, {
+          key: 'getElementHandler',
+          value: function getElementHandler(target, propertyName) {
+            var tagName,
+                lookup = this.elementHandlerLookup;
+            if (target.tagName) {
+              tagName = target.tagName.toLowerCase();
+              if (lookup[tagName] && lookup[tagName][propertyName]) {
+                return lookup[tagName][propertyName];
+              }
+              if (propertyName === 'textContent' || propertyName === 'innerHTML') {
+                return lookup.input.value;
+              }
+            }
+
+            return null;
+          }
+        }, {
+          key: 'addEventListener',
+          value: function addEventListener(target, targetEvent, callback, delegate) {
+            return (this.eventStrategyLookup[targetEvent] || this.defaultEventStrategy).subscribe(target, targetEvent, callback, delegate);
+          }
+        }]);
+
+        return EventManager;
+      })();
+
+      _export('EventManager', EventManager);
+    }
+  };
+});
