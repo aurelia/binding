ae5c800ec51e67f6660ee7f84d7cafa8c2adff4b
Jeremy Danyow <jdanyow@gmail.com>
2015-09-13 16:11:40 -0400

    test(ModifyMapObserver): add test

---
+++ test/map-observation.spec.js
@@ -0,0 +1,25 @@
+import {TaskQueue} from 'aurelia-task-queue';
+import {getMapObserver} from '../src/map-observation';
+
+describe('ModifyMapObserver', () => {
+  let taskQueue;
+  beforeAll(() => {
+    taskQueue = new TaskQueue();
+  });
+
+  it('identifies set with falsey oldValue as an "update"', done => {
+    let map = new Map();
+    map.set('foo', 0); // falsey old value.
+    let observer = getMapObserver(taskQueue, map);
+    let callback = jasmine.createSpy('callback');
+    observer.subscribe(callback);
+    map.set('foo', 'bar');
+    taskQueue.queueMicroTask({
+      call: () => {
+        expect(callback).toHaveBeenCalledWith([{ type: 'update', object: map, key: 'foo', oldValue: 0 }], undefined);
+        observer.unsubscribe(callback);
+        done();
+      }
+    });
+  });
+});
