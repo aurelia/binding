f34972dc5187c1762b2ac749c3d92fbb3669dad9
Jeremy Danyow <jdanyow@gmail.com>
2015-12-27 22:07:07 -0500

    feat(Array): observe array prototype methods

--- src/array-observation.js
+++ src/array-observation.js
@@ -2,94 +2,123 @@ import {projectArraySplices} from './array-change-records';
 import {ModifyCollectionObserver, CollectionLengthObserver} from './collection-observation';
 import {subscriberCollection} from './subscriber-collection';
 
-var arrayProto = Array.prototype;
+let pop = Array.prototype.pop;
+let push = Array.prototype.push;
+let reverse = Array.prototype.reverse;
+let shift = Array.prototype.shift;
+let sort = Array.prototype.sort;
+let splice = Array.prototype.splice;
+let unshift = Array.prototype.unshift;
 
-export function getArrayObserver(taskQueue, array){
+Array.prototype.pop = function() {
+  let methodCallResult = pop.apply(this, arguments);
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.addChangeRecord({
+      type: 'delete',
+      object: this,
+      name: this.length,
+      oldValue: methodCallResult
+    });
+  }
+  return methodCallResult;
+}
+
+Array.prototype.push = function() {
+  let methodCallResult = push.apply(this, arguments);
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.addChangeRecord({
+      type: 'splice',
+      object: this,
+      index: this.length - arguments.length,
+      removed: [],
+      addedCount: arguments.length
+    });
+  }
+  return methodCallResult;
+}
+
+Array.prototype.reverse = function() {
+  let oldArray;
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.flushChangeRecords();
+    oldArray = this.slice();
+  }
+  let methodCallResult = reverse.apply(this, arguments);
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.reset(oldArray);
+  }
+  return methodCallResult;
+}
+
+Array.prototype.shift = function() {
+  let methodCallResult = shift.apply(this, arguments);
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.addChangeRecord({
+      type: 'delete',
+      object: this,
+      name: 0,
+      oldValue: methodCallResult
+    });
+  }
+  return methodCallResult
+};
+
+Array.prototype.sort = function() {
+  let oldArray;
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.flushChangeRecords();
+    oldArray = this.slice();
+  }
+  let methodCallResult = sort.apply(this, arguments);
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.reset(oldArray);
+  }
+  return methodCallResult;
+};
+
+Array.prototype.splice = function() {
+  let methodCallResult = splice.apply(this, arguments);
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.addChangeRecord({
+      type: 'splice',
+      object: this,
+      index: arguments[0],
+      removed: methodCallResult,
+      addedCount: arguments.length > 2 ? arguments.length - 2 : 0
+    });
+  }
+  return methodCallResult;
+};
+
+Array.prototype.unshift = function() {
+  let methodCallResult = unshift.apply(this, arguments);
+  if (this.__arrayObserver !== undefined) {
+    this.__arrayObserver.addChangeRecord({
+      type: 'splice',
+      object: this,
+      index: 0,
+      removed: [],
+      addedCount: arguments.length
+    });
+  }
+  return methodCallResult;
+};
+
+export function getArrayObserver(taskQueue, array) {
   return ModifyArrayObserver.create(taskQueue, array);
 }
 
 class ModifyArrayObserver extends ModifyCollectionObserver {
-  constructor(taskQueue, array){
+  constructor(taskQueue, array) {
     super(taskQueue, array);
   }
 
-  static create(taskQueue, array){
-    var observer = new ModifyArrayObserver(taskQueue, array);
-
-    array['pop'] = function(){
-      var methodCallResult = arrayProto['pop'].apply(array, arguments);
-      observer.addChangeRecord({
-       type: 'delete',
-       object: array,
-       name: array.length,
-       oldValue: methodCallResult
-      });
-      return methodCallResult;
-    }
-
-    array['push'] = function(){
-      var methodCallResult = arrayProto['push'].apply(array, arguments);
-      observer.addChangeRecord({
-       type: 'splice',
-       object: array,
-       index: array.length - arguments.length,
-       removed: [],
-       addedCount: arguments.length
-      });
-      return methodCallResult;
-    }
-
-    array['reverse'] = function(){
-      observer.flushChangeRecords();
-      var oldArray = array.slice();
-      var methodCallResult = arrayProto['reverse'].apply(array, arguments);
-      observer.reset(oldArray);
-      return methodCallResult;
-    }
-
-    array['shift'] = function() {
-      var methodCallResult = arrayProto['shift'].apply(array, arguments);
-      observer.addChangeRecord({
-       type: 'delete',
-       object: array,
-       name: 0,
-       oldValue: methodCallResult
-      });
-      return methodCallResult
-    };
-
-    array['sort'] = function() {
-      observer.flushChangeRecords();
-      var oldArray = array.slice();
-      var methodCallResult = arrayProto['sort'].apply(array, arguments);
-      observer.reset(oldArray);
-      return methodCallResult;
-    };
-
-    array['splice'] = function() {
-      var methodCallResult = arrayProto['splice'].apply(array, arguments);
-      observer.addChangeRecord({
-       type: 'splice',
-       object: array,
-       index: arguments[0],
-       removed: methodCallResult,
-       addedCount: arguments.length > 2 ? arguments.length - 2 : 0
-      });
-      return methodCallResult;
-    };
-
-    array['unshift'] = function() {
-      var methodCallResult = arrayProto['unshift'].apply(array, arguments);
-      observer.addChangeRecord({
-       type: 'splice',
-       object: array,
-       index: 0,
-       removed: [],
-       addedCount: arguments.length
-      });
-      return methodCallResult;
-    };
-
+  static create(taskQueue, array) {
+    let observer = new ModifyArrayObserver(taskQueue, array);
+    Object.defineProperty(
+      array,
+      '__arrayObserver',
+      { value: observer, enumerable: false, configurable: false });
     return observer;
   }
 }
