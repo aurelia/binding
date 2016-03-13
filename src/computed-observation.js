461a3d56ced2a51f705e6c069ac59e97771e93c6
Jeremy Danyow <jdanyow@gmail.com>
2016-03-13 16:18:17 -0400

    feat(computedFrom): support expressions fixes #149

--- src/computed-observation.js
+++ src/computed-observation.js
@@ -1,62 +1,6 @@
-import {subscriberCollection} from './subscriber-collection';
-
-const computedContext = 'ComputedPropertyObserver';
-
-@subscriberCollection()
-export class ComputedPropertyObserver {
-  constructor(obj, propertyName, descriptor, observerLocator) {
-    this.obj = obj;
-    this.propertyName = propertyName;
-    this.descriptor = descriptor;
-    this.observerLocator = observerLocator;
-  }
-
-  getValue(){
-    return this.obj[this.propertyName];
-  }
-
-  setValue(newValue){
-    this.obj[this.propertyName] = newValue;
-  }
-
-  call(context) {
-    let newValue = this.getValue();
-    if (this.oldValue === newValue)
-      return;
-    this.callSubscribers(newValue, this.oldValue);
-    this.oldValue = newValue;
-    return;
-  }
-
-  subscribe(context, callable) {
-    if (!this.hasSubscribers()) {
-      this.oldValue = this.getValue();
-
-      let dependencies = this.descriptor.get.dependencies;
-      this.observers = [];
-      for (let i = 0, ii = dependencies.length; i < ii; i++) {
-        let observer = this.observerLocator.getObserver(this.obj, dependencies[i]);
-        // todo:  consider throwing when a dependency's observer is an instance of DirtyCheckProperty.
-        this.observers.push(observer);
-        observer.subscribe(computedContext, this);
-      }
-    }
-
-    this.addSubscriber(context, callable);
-  }
-
-  unsubscribe(context, callable) {
-    if (this.removeSubscriber(context, callable) && !this.hasSubscribers()) {
-      this.oldValue = undefined;
-
-      let i = this.observers.length;
-      while(i--) {
-        this.observers[i].unsubscribe(computedContext, this);
-      }
-      this.observers = null;
-    }
-  }
-}
+import {Expression} from './ast';
+import {createOverrideContext} from './scope';
+import {ExpressionObserver} from './expression-observer';
 
 export function hasDeclaredDependencies(descriptor) {
   return descriptor && descriptor.get && descriptor.get.dependencies && descriptor.get.dependencies.length > 0;
@@ -72,4 +16,48 @@ export function computedFrom(...rest){
     descriptor.get.dependencies = rest;
     return descriptor;
   }
+}
+
+export class ComputedExpression extends Expression {
+  constructor(name, dependencies) {
+    super();
+
+    this.name = name;
+    this.dependencies = dependencies;
+    this.isAssignable = true;
+  }
+
+  evaluate(scope, lookupFunctions) {
+    return scope.bindingContext[this.name];
+  }
+
+  assign(scope, value) {
+    scope.bindingContext[this.name] = value;
+  }
+
+  accept(visitor) {
+    throw new Error('not implemented');
+  }
+
+  connect(binding, scope) {
+    let dependencies = this.dependencies;
+    let i = dependencies.length;
+    while (i--) {
+      dependencies[i].connect(binding, scope);
+    }
+  }
+}
+
+export function createComputedObserver(obj, propertyName, descriptor, observerLocator) {
+  let dependencies = descriptor.get.dependencies;
+  if (!(dependencies instanceof ComputedExpression)) {
+    let i = dependencies.length;
+    while (i--) {
+      dependencies[i] = observerLocator.parser.parse(dependencies[i]);
+    }
+    dependencies = descriptor.get.dependencies = new ComputedExpression(propertyName, dependencies);
+  }
+
+  let scope = { bindingContext: obj, overrideContext: createOverrideContext(obj) };
+  return new ExpressionObserver(scope, dependencies, observerLocator);
 }
