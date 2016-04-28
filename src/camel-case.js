315cfaa229c13da8355ee1dec201a09c7df507db
Jeremy Danyow <jdanyow@gmail.com>
2016-04-28 07:12:48 -0400

    feat(camelCase): handle hyphenated names

--- src/camel-case.js
+++ src/camel-case.js
@@ -1,3 +1,11 @@
+const map = Object.create(null);
+
 export function camelCase(name) {
-  return name.charAt(0).toLowerCase() + name.slice(1);
+  if (name in map) {
+    return map[name];
+  }
+  const result = name.charAt(0).toLowerCase()
+    + name.slice(1).replace(/[_.-](\w|$)/g, (_, x) => x.toUpperCase());
+  map[name] = result;
+  return result;
 }
