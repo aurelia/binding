047f1c33e49911c7f96d45007a0da54e7127870a
doktordirk <doktordirk@outlook.de>
2016-10-01 11:44:04 +0200

    chore(build): cleanup path.js

--- build/paths.js
+++ build/paths.js
@@ -1,22 +1,6 @@
 var path = require('path');
 var fs = require('fs');
 
-var appRoot = 'src/';
-var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-
-module.exports = {
-  root: appRoot,
-  source: appRoot + '**/*.js',
-  html: appRoot + '**/*.html',
-  style: 'styles/**/*.css',
-  output: 'dist/',
-  doc:'./doc',
-  e2eSpecsSrc: 'test/e2e/src/*.js',
-  e2eSpecsDist: 'test/e2e/dist/',
-  packageName: pkg.name
-};var path = require('path');
-var fs = require('fs');
-
 // hide warning //
 var emitter = require('events');
 emitter.defaultMaxListeners = 20;
