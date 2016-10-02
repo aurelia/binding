0b6a2cb4ebd95d01db8630ba47daae83c04c7f88
Gheoan <xvip10@ymail.com>
2016-10-02 04:12:05 +0300

    fix(observable): inner property should be non-enumerable
    Fix #505.

--- test/decorator-observable.spec.js
+++ test/decorator-observable.spec.js
@@ -1,6 +1,8 @@
 import './setup';
 import {observable} from '../src/decorator-observable.js';
 import {decorators} from 'aurelia-metadata';
+import {SetterObserver} from '../src/property-observation';
+import {Logger} from 'aurelia-logging';
 
 describe('observable decorator', () => {
   const oldValue = 'old';
@@ -113,10 +115,94 @@ describe('observable decorator', () => {
     const instance = new class {
       @observable value;
     };
-    const keys = [];
-    for (let p in instance) {
-      keys.push(p);
-    }
-    expect(keys).toContain('value');
+    const descriptor = Object.getOwnPropertyDescriptor(Object.getPrototypeOf(instance), 'value');
+
+    expect(descriptor.enumerable).toBe(true);
+    expect(typeof descriptor.set).toBe('function');
+    expect(typeof descriptor.get).toBe('function');
+  });
+
+  describe('private property', () => {
+    describe(`when public property's value is not changed`, () => {
+      const instance = new class {
+        @observable value;
+      };
+      const prototype = Object.getPrototypeOf(instance);
+
+      it('should exist on the prototype', () => {
+        expect(prototype.hasOwnProperty('_value')).toBe(true);
+      });
+
+      it('should be nonenumerable', () => {
+        expect(prototype.propertyIsEnumerable('_value')).toBe(false);
+      });
+
+      describe('observation', () => {
+        const observer = new SetterObserver(null, prototype, '_value');
+
+        it('should convert to accessors without warning', () => {
+          spyOn(Logger.prototype, 'warn');
+          observer.convertProperty();
+          expect(Logger.prototype.warn).not.toHaveBeenCalled();
+        });
+
+        it('should exist', () => {
+          const descriptor = Object.getOwnPropertyDescriptor(prototype, '_value');
+          expect(typeof descriptor.get).toBe('function');
+          expect(typeof descriptor.set).toBe('function');
+        });
+
+        it('should be nonenumerable', () => {
+          expect(prototype.propertyIsEnumerable('_value')).toBe(false);
+        });
+      });
+    });
+
+    describe(`when public property's value is changed`, () => {
+      const instance = new class {
+        @observable value;
+      };
+      instance.value = newValue;
+
+      it('should exist on the instance', () => {
+        expect(instance.hasOwnProperty('_value')).toBe(true);
+      });
+
+      it('should be nonenumerable', () => {
+        expect(instance.propertyIsEnumerable('_value')).toBe(false);
+      });
+
+      describe('observation', () => {
+        const observer = new SetterObserver(null, instance, '_value');
+
+        it('should convert to accessors without warning', () => {
+          spyOn(Logger.prototype, 'warn');
+          observer.convertProperty();
+          expect(Logger.prototype.warn).not.toHaveBeenCalled();
+        });
+
+        it('should exist', () => {
+          const descriptor = Object.getOwnPropertyDescriptor(instance, '_value');
+          expect(typeof descriptor.get).toBe('function');
+          expect(typeof descriptor.set).toBe('function');
+        });
+
+        it('should be nonenumerable', () => {
+          expect(instance.propertyIsEnumerable('_value')).toBe(false);
+        });
+      });
+    });
+
+    it('should have distinct values between instances', () => {
+      class MyClass {
+        @observable value = oldValue;
+      }
+
+      const instance1 = new MyClass();
+      const instance2 = new MyClass();
+
+      instance1.value = newValue;
+      expect(instance2.value).toBe(oldValue);
+    });
   });
 });
