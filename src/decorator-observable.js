521270b019aba03f1a68e038407518b26691aaf0
Gheorghe Anghelescu <xvip10@ymail.com>
2016-10-05 02:55:55 +0300

    fix(observable): backing property should not be enumerable
    Fixes: #505.

--- src/decorator-observable.js
+++ src/decorator-observable.js
@@ -9,6 +9,11 @@ export function observable(targetOrConfig: any, key: string, descriptor?: Proper
 
     // use a convention to compute the inner property name
     let innerPropertyName = `_${key}`;
+    const innerPropertyDescriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: false,
+      writable: true,
+    };
 
     // determine callback name based on config or convention.
     const callbackName = (config && config.changeHandler) || `${key}Changed`;
@@ -18,7 +23,7 @@ export function observable(targetOrConfig: any, key: string, descriptor?: Proper
 
       // set the initial value of the property if it is defined.
       if (typeof descriptor.initializer === 'function') {
-        target[innerPropertyName] = descriptor.initializer();
+        innerPropertyDescriptor.value = descriptor.initializer();
       }
     } else {
       // there is no descriptor if the target was a field in TS (although Babel provides one),
@@ -35,11 +40,18 @@ export function observable(targetOrConfig: any, key: string, descriptor?: Proper
     delete descriptor.writable;
     delete descriptor.initializer;
 
+    // Add the inner property on the prototype.
+    Reflect.defineProperty(target, innerPropertyName, innerPropertyDescriptor);
+
     // add the getter and setter to the property descriptor.
     descriptor.get = function() { return this[innerPropertyName]; };
     descriptor.set = function(newValue) {
       let oldValue = this[innerPropertyName];
+
+      // Add the inner property on the instance and make it nonenumerable.
       this[innerPropertyName] = newValue;
+      Reflect.defineProperty(this, innerPropertyName, { enumerable: false });
+
       if (this[callbackName]) {
         this[callbackName](newValue, oldValue, key);
       }
