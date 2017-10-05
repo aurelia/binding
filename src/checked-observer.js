b6565eb116c212188feec296ae4782c3c1d95c89
bigopon <bigopon.777@gmail.com>
2017-10-05 13:46:25 +1100

    refactor(Observation): use handleEvent instead of bind

--- src/checked-observer.js
+++ src/checked-observer.js
@@ -111,9 +111,13 @@ export class CheckedObserver {
     this.callSubscribers(newValue, oldValue);
   }
 
+  handleEvent() {
+    this.synchronizeValue();
+  }
+
   subscribe(context, callable) {
     if (!this.hasSubscribers()) {
-      this.disposeHandler = this.handler.subscribe(this.element, this.synchronizeValue.bind(this, false));
+      this.disposeHandler = this.handler.subscribe(this.element, this);
     }
     this.addSubscriber(context, callable);
   }
