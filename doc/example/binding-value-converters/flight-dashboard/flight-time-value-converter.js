a30231b22ad113afd5e189886ef36359ff805a0c
bigopon <bigopon.777@gmail.com>
2017-10-09 20:49:51 +1100

    doc(signals): doc for value converter signals

---
+++ doc/example/binding-value-converters/flight-dashboard/flight-time-value-converter.js
@@ -0,0 +1,8 @@
+export class FlightTimeValueConverter {
+  signals = 'locale-changed'
+
+  toView(val) {
+    let newVal = val instanceof Date ? val.toLocaleString(window.currentLocale) : val === null ? '' : val; // eslint-disable-line
+    return newVal;
+  }
+}
